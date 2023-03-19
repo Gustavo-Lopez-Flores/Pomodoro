9e47a9cd59858f5b35731fd7cb47f359bc6c3335
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-03-19 08:39:54 -0400

    alternar modos por botao e ao fim da contagem

--- app/src/main/java/com/example/pomodoro/MainActivity.java
+++ app/src/main/java/com/example/pomodoro/MainActivity.java
@@ -14,14 +14,19 @@ public class MainActivity extends AppCompatActivity {
 
     /* Timer do Pomodoro */
     private static long POMODORO_TIME = 1500000; // 25min
+    private static long LONG_BREAK_TIME = 600000; // 10min
+    private static long SHORT_BREAK_TIME = 300000; // 5min
+    private int statusModeTimer = 0;
 
     /* Text e Button */
     private TextView textTimer;
+    private TextView textStatusMode;
     private Button buttonStartPause;
+    private Button buttonStatusMode;
 
     /* Contador */
     private CountDownTimer tempoCronometro;
-    private boolean statusTimer;
+    private boolean statusStartPause;
     private long tempoMilissegundos = POMODORO_TIME;
 
     @Override
@@ -30,12 +35,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textTimer = findViewById(R.id.textTimer);
+        textStatusMode = findViewById(R.id.textStatusMode);
         buttonStartPause = findViewById(R.id.buttonStartPause);
+        buttonStatusMode = findViewById(R.id.buttonStatusMode);
+
+        buttonStatusMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                statusModeTimer = (statusModeTimer+1) % 3;
+                seeStatus();
+                timeCounter();
+            }
+        });
 
         buttonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                if(statusTimer){
+                buttonStatusMode.setVisibility(View.INVISIBLE);
+                if(statusStartPause){
                     pauseTimer();
                 }else{
                     startTimer();
@@ -43,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        contagemRegressiva();
+        timeCounter();
     }
 
     private void startTimer(){
@@ -51,28 +68,53 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onTick(long tempoRestante) {
                 tempoMilissegundos = tempoRestante;
-                contagemRegressiva();
+                timeCounter();
             }
             @Override
             public void onFinish() {
-                statusTimer = false;
+                if(statusModeTimer == 0){
+                    statusModeTimer++;
+                }else{
+                    statusModeTimer=0;
+                }
+                seeStatus();
+                timeCounter();
+                statusStartPause = false;
                 buttonStartPause.setText("INICIAR");
+                buttonStatusMode.setVisibility(View.VISIBLE);
             }
         }.start();
-        statusTimer = true;
+        statusStartPause = true;
         buttonStartPause.setText("PAUSAR");
     }
 
     private void pauseTimer(){
         tempoCronometro.cancel();
-        statusTimer = false;
+        statusStartPause = false;
         buttonStartPause.setText("CONTINUAR");
     }
 
-    private void contagemRegressiva(){
+    private void timeCounter(){
         int minutos = (int) (tempoMilissegundos / 1000) / 60;
         int segundos = (int) (tempoMilissegundos / 1000) % 60;
         String tempoFormatado = String.format(Locale.getDefault(), "%02d:%02d", minutos, segundos);
         textTimer.setText(tempoFormatado);
+    }
+
+    private void seeStatus(){
+        switch (statusModeTimer){
+            case 0:
+                textStatusMode.setText("POMODORO");
+                tempoMilissegundos = POMODORO_TIME;
+                break;
+            case 1:
+                textStatusMode.setText("PAUSA CURTA");
+                tempoMilissegundos = SHORT_BREAK_TIME;
+                break;
+            case 2:
+                textStatusMode.setText("PAUSA LONGA");
+                tempoMilissegundos = LONG_BREAK_TIME;
+                break;
+        }
     }
 }
