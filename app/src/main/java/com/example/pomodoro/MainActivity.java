6ea7b6a0920974971987e40f00b28f87d4b14f90
Gustavo Lopez Flores <gustavo130403@gmail.com>
2023-03-18 22:32:41 -0400

    funcao pausar

--- app/src/main/java/com/example/pomodoro/MainActivity.java
+++ app/src/main/java/com/example/pomodoro/MainActivity.java
@@ -17,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     /* Text e Button */
     private TextView textTimer;
-    private Button buttonStart;
+    private Button buttonStartPause;
 
     /* Contador */
     private CountDownTimer tempoCronometro;
+    private boolean statusTimer;
     private long tempoMilissegundos = POMODORO_TIME;
 
     @Override
@@ -29,18 +30,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textTimer = findViewById(R.id.textTimer);
-        buttonStart = findViewById(R.id.buttonStart);
+        buttonStartPause = findViewById(R.id.buttonStartPause);
 
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        buttonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                startTimer();
+                if(statusTimer){
+                    pauseTimer();
+                }else{
+                    startTimer();
+                }
             }
         });
 
+        contagemRegressiva();
     }
 
-    public void startTimer(){
+    private void startTimer(){
         tempoCronometro = new CountDownTimer(tempoMilissegundos, 1000){
             @Override
             public void onTick(long tempoRestante) {
@@ -49,13 +55,21 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onFinish() {
-
+                statusTimer = false;
+                buttonStartPause.setText("INICIAR");
             }
         }.start();
-        buttonStart.setText("PAUSAR"); // próxima feature
+        statusTimer = true;
+        buttonStartPause.setText("PAUSAR");
     }
 
-    public void contagemRegressiva(){
+    private void pauseTimer(){
+        tempoCronometro.cancel();
+        statusTimer = false;
+        buttonStartPause.setText("CONTINUAR");
+    }
+
+    private void contagemRegressiva(){
         int minutos = (int) (tempoMilissegundos / 1000) / 60;
         int segundos = (int) (tempoMilissegundos / 1000) % 60;
         String tempoFormatado = String.format(Locale.getDefault(), "%02d:%02d", minutos, segundos);
